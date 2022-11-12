e91641154ea3558d3d2a836abae53dcf0ccdf864
yjrubixcube <jyunwangt99@gmail.com>
2022-11-12 23:29:06 +0800

    added start buttons

--- sketch.js
+++ sketch.js
@@ -6,51 +6,91 @@ let screen = [1080, 540];
 // bar vars const
 let bar_x = 440, bar_y = 500;
 let bar_width = 200, bar_height = 10;
+// let bar = [bar_x, bar_y, bar_width, bar_height];
 
 // img vars
-// let main_figure;
-// let max_width = screen[0]/5, max_height = screen[1]/5;
-// let min_width = screen[0]/10, min_height = screen[1]/10;
-// let cur_width = max_width, cur_height = max_height;
+let main_figure, sound_figure;
 
 // button vars
-let button_x = 525, button_y = 490;
-let button_width = 30, button_height = 30;
+let button_x = 525, button_y = 455;
+let button_width = 30, button_height = 100;
+// let button = [button_x, button_y, button_width, button_height];
+
+class Rectangle {
+	constructor(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	pressed_or_clicked() {
+		if (mouse_over(this)){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	render(col) {
+		fill(col);
+		rect(this.x, this.y, this.width, this.height);
+	}
+}
+
+let bar = new Rectangle(bar_x, bar_y, bar_width, bar_height);
+let button = new Rectangle(button_x, button_y, button_width, button_height);
+let start = new Rectangle(200, 500, 40, 40);
 
 function setup() {
 	createCanvas(screen[0], screen[1]);
-	// fill(0, 255, 255);
-}
-// function move_mouth() {
-// 	main_figure = loadImage("images/test.png");
-// }
 
+	main_figure = loadImage("images/beast_waiting.jpg");
+	sound_figure = loadImage("images/beast_roar.jpg");
+}
+
+let play = false;
+let opach = 0;
 
 function draw() {
 	background(255);
 
+	tint(255, opach);
+	image(main_figure, 0, 0, width, height, 0, 0, main_figure.width, main_figure.height, COVER);
+	tint(255, 255-opach);
+	image(sound_figure, 0, 0, width, height, 0, 0, sound_figure.width, sound_figure.height, COVER);
+	start.render(color(0, 255, 0));
 	// change statement to pd things
 	if (mouseIsPressed) {
-		if (mouse_over(button_x, button_x + button_width, button_y, button_y + button_height) === true) {
-			button_x = mouseX - button_width/2;
+		// play = !play;
+		fill(color(255,255,0));
+		rect(0, 0, 200, 200);
+		if (button.pressed_or_clicked()) {
+			fill(color(255,0,0));
+			rect(200, 0, 200, 200);
+			button.x = mouseX - button.width/2;
 		}
 	}
-	// else{
-	// 	// if (cur_width > min_width){
-	// 	// 	cur_width -= 2;
-	// 	// 	cur_height -= 1;
-	// 	// }
-	// }
 
-	// image(main_figure, mouseX, mouseY, cur_width, cur_height);
-
-	render_rect(bar_x, bar_y, bar_width, bar_height, color(0,0,0));
-	render_rect(button_x, button_y, button_width, button_height, color(255,0,0));
+	if (play) {
+		if (opach >= 0) {
+			opach -= 15;
+		}
+	}
+	else{
+		if (opach <= 255) {
+			opach += 15;
+		}
+	}
+	
+	bar.render(color(0));
+	button.render(color(255, 0, 0));
 
 }
 
-function mouse_over(x_start, x_end, y_start, y_end) {
-	if (mouseX >= x_start && mouseX <= x_end && mouseY >= y_start && mouseY <= y_end) {
+function mouse_over(shape) {
+	if (mouseX >= shape.x && mouseX <= shape.x + shape.width && mouseY >= shape.y && mouseY <= shape.y + shape.height) {
 		return true;
 	}
 	else {
@@ -58,7 +98,9 @@ function mouse_over(x_start, x_end, y_start, y_end) {
 	}
 }
 
-function render_rect(x_pos, y_pos, width, height, color) {
-	fill(color);
-	rect(x_pos, y_pos, width, height);
+
+function mouseClicked() {
+	if (start.pressed_or_clicked()) {
+		play = !play;
+	}
 }
